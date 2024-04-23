e6dc74873a97c59294bbfbe0a2fd2a00cd97e7d1
Jona9290 <jona9290@gmail.com>
2024-04-23 15:19:28 +0200

    Entered Cold Code to Match_Feed

---
+++ Database/DatingApp/Controllers/MainController.cs
@@ -0,0 +1,4 @@
+using DatingApp.DataRepository;
+using DatingApp.Model;
+using Microsoft.AspNetCore.Mvc;
+
--- Database/DatingApp/Model/Match_feed.cs
+++ Database/DatingApp/Model/Match_feed.cs
@@ -4,11 +4,26 @@
     {
         //first thing we should do is to find another profile, make sure the profile is not either liked or matched. 
     public int DisplayProfile() {  return 0; }
-
+        //Add While loop
+        //Get profile through repository of foreign ID
+            //Check they are not in list of matches or likes(Consider making this a function for itself)
+            //if True - find new ID, if False - continue
+        //await Liking or disliking of profile
+        //if like call IsLike function
+            //continue
+        //if dislike run again
+   
     public bool IsMatch() { return false; }
+        //pull foreign ID from liked persons liked and check if like is mutual
+        //if true create match and push it through repository
+        //delete like on both IDs in Repository
+        //if false return false
 
-    public bool isLike(string s) { return false; }
-
+    public bool IsLike(string s) { return false; }
+        //check if like is match call IsMatch function
+        //if return false add like to like list through repository
+            //return false
+        //if true return true
     }
 
     //testing 
