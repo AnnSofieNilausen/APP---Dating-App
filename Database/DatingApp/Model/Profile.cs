d2e28abeb50366d7d2f4c014e93abb7b602d4c92
Jona9290 <jona9290@gmail.com>
2024-04-26 14:56:58 +0200

    profile update

--- Database/DatingApp/Model/Profile.cs
+++ Database/DatingApp/Model/Profile.cs
@@ -9,22 +9,45 @@ namespace DatingApp.Model
 			ID = id;
 		}
 
-		public Profile() { }
+        public Profile(int fname)
+        {
+            Fname = fname;
+        }
+
+        public Profile() { }
 
 		[JsonPropertyName("id")]
 		public int ID { get; set; }
-		[JsonPropertyName("firstName")]
+		[JsonPropertyName("FName")]
 		public string FName { get; set; }
-		[JsonPropertyName("lastName")]
+		[JsonPropertyName("LName")]
 		public string LName { get; set; }
-		[JsonPropertyName("studyProgramID")]
-		public int StudyProgramID { get; set; }
 		[JsonPropertyName("dob")]
 		public DateTime DOB { get; set; }
 		[JsonPropertyName("email")]
 		public string Email { get; set; }
 		[JsonPropertyName("phone")]
 		public string Phone { get; set; }
-	}
+        [JsonPropertyName("username")]
+        public string username { get; set; }
+        [JsonPropertyName("password")]
+        public string password { get; set; }
+        [JsonPropertyName("sexualOrientation")]
+        public string sexualOrientation { get; set; }
+        [JsonPropertyName("bio")]
+        public string bio { get; set; }
+        [JsonPropertyName("searchingFor")]
+        public string searchingFor { get; set; }
+        [JsonPropertyName("interests")]
+        public string interests { get; set; }
+        [JsonPropertyName("occupation")]
+        public string occupation { get; set; }
+        [JsonPropertyName("pictures")]
+        public string pictures { get; set; }
+        [JsonPropertyName("instagram")]
+        public string instagram { get; set; }
+        [JsonPropertyName("snapchat")]
+        public string snapchat { get; set; }
+    }
 }
 
