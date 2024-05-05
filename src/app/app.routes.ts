8802496fb88444bff692d5512a1d52525e94916a
Ann Sofie Nilausen <annsofienilausen@MacBook-Pro-tilhrende-Ann.local>
2024-05-05 15:35:27 +0200

    fixed errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,22 @@
-import { Routes } from '@angular/router';
-import { StudentListComponent } from './student-list/student-list.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { EditStudentComponent } from './edit-student/edit-student.component';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '../app/components/login/login.component';
+import { MutualMatchesListComponent } from '../app/components/mutual-matches-list/mutual-matches-list.component';
+import { MatchingComponent } from '../app/components/potential-matches-list/potential-matches-list.component';
+import { ProfileComponent } from '../app/components/profile/profile.component';
 
 export const routes: Routes = [
-   { path: "students", component: StudentListComponent },
-   { path: "dashboard", component: DashboardComponent },
-   { path: "edit-student/:id", component: EditStudentComponent },
-   { path: "**", component: StudentListComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'mutual-matches', component: MutualMatchesListComponent },
+  { path: 'potential-matches', component: MatchingComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
+
+/*
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+*/
